767740ece6d87bc951fba65250937002f4c43365
Adam Christie <fractos@gmail.com>
2019-10-03 17:26:49 +0100

    use index.json instead of root.json.

--- app/collection.py
+++ app/collection.py
@@ -6,7 +6,7 @@ import settings
 
 collection = {
   "@context": "http://iiif.io/api/presentation/2/context.json",
-  "@id": f"{settings.URL_PREFIX}/root.json",
+  "@id": f"{settings.URL_PREFIX}/index.json",
   "@type": "sc:Collection",
   "label": "RCAMW",
   "viewingHint": "top",
@@ -32,5 +32,5 @@ for filename in filenames:
     })
 
 
-with open(join(settings.DATA_FOLDER, "root.json"), "w") as new_file:
+with open(join(settings.DATA_FOLDER, "index.json"), "w") as new_file:
     new_file.write(json.dumps(collection, indent=4, sort_keys=True))
